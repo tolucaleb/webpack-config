3aac2a7be2a1118f30267f2faacda80f10d89f6d
Caleb Tolu <israel.fabulous@gmail.com>
2017-03-28 15:30:02 +0100

    updated webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,107 +1,132 @@
-const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 let config;
-if(process.env.NODE_ENV == 'development'){
-    config = {
-        entry: {
-            app : ['./js/index']
-        },
-        output: {
-            publicPath: '/',
-            filename: '[name]-bundle.js'
-        },
-        devtool: 'source-map',
-        module: {
-            rules: [
+if (process.env.NODE_ENV === 'development') {
+  config = {
+    entry: {
+      app: ['./js/index'],
+    },
+    output: {
+      publicPath: '/',
+      filename: '[name]-bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
 
-                {
-                    test:/\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    loader:['react-hot-loader','babel-loader']
-                },
-                {
-                    test:/\.(css|scss|sass)/,
-                    loader:'css-loader!postcss-loader!less-loader!sass-loader'
-                }
-            ]
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: ['babel-loader'],
         },
-        devServer: {
-            historyApiFallback: true,
-            contentBase: './'
-        }
-
-    };
-
-}
-else{
-    config = {
-        entry: {
-            app : ['./js/index']
+        {
+          test: /\.(css|scss|sass)/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            loader: 'css-loader!postcss-loader!less-loader!sass-loader',
+            options: { sourceMap: false, minimize: false },
+          }),
         },
-        output: {
-            path: './public',
-            filename: '[name]-bundle.js'
+        {
+          test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        }, {
+          test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        }, {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+        }, {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file-loader',
+        }, {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
         },
-        devtool: 'source-map',
-        module: {
-            rules: [
-
-                {
-                    test:/\.(js|jsx)$/,
-                    exclude: /node_modules/,
-                    loader:['babel-loader']
-                },
-                {
-                    test:/\.(css|scss|sass)/,
-                    loader: ExtractTextPlugin.extract({
-                        fallback: 'style-loader',
-                        loader:'css-loader!postcss-loader!less-loader!sass-loader',
-                        options: {sourceMap: true,minimize : true }
-                    })
-                },
-                {
-                    test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-                    loader: "url-loader?limit=10000&mimetype=application/font-woff"
-                }, {
-                    test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-                    loader: "url-loader?limit=10000&mimetype=application/font-woff"
-                }, {
-                    test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-                    loader: "url-loader?limit=10000&mimetype=application/octet-stream"
-                }, {
-                    test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-                    loader: "file-loader"
-                }, {
-                    test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-                    loader: "url-loader?limit=10000&mimetype=image/svg+xml"
-                }
-
-            ]
+      ],
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new ExtractTextPlugin('bundle.css'),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('development'),
         },
-        plugins: [
-            new webpack.HotModuleReplacementPlugin(),
-            new ExtractTextPlugin( "bundle.css"),
-            new OptimizeCssAssetsPlugin({
-                assetNameRegExp:/\.css$/,
-                cssProcessorOptions:{
-                    discardComments:{
-                        removeAll:true}
-                },
-                canPrint:true
-            }),
-            new webpack.DefinePlugin({
-                'process.env': {
-                    NODE_ENV: JSON.stringify('production')
-                }
-            }),
-            new webpack.optimize.UglifyJsPlugin({
-                compress: { warnings: false }
-            })
-        ]
-    };
+      }),
+    ],
+    devServer: {
+      historyApiFallback: true,
+      contentBase: './',
+    },
 
+  };
+} else {
+  config = {
+    entry: {
+      app: ['./js/index'],
+    },
+    output: {
+      path: './public',
+      filename: '[name]-bundle.js',
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: ['babel-loader'],
+        },
+        {
+          test: /\.(css|scss|sass)/,
+          loader: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            loader: 'css-loader!postcss-loader!less-loader!sass-loader',
+            options: { sourceMap: true, minimize: true },
+          }),
+        },
+        {
+          test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        }, {
+          test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+        }, {
+          test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+        }, {
+          test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'file-loader',
+        }, {
+          test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+          loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
+        },
+      ],
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new ExtractTextPlugin('bundle.css'),
+      new OptimizeCssAssetsPlugin({
+        assetNameRegExp: /\.css$/,
+        cssProcessorOptions: {
+          discardComments: {
+            removeAll: true },
+        },
+        canPrint: true,
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production'),
+        },
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false },
+      }),
+    ],
+  };
 }
 
 
