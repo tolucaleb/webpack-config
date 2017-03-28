38aaac5612bff70c2367c397f7c6d3b5a7319197
Caleb Tolu <israel.fabulous@gmail.com>
2017-03-28 15:41:48 +0100

    Added Bootstrap,FontAwesome

--- js/index.js
+++ js/index.js
@@ -1,25 +1,3 @@
-/**
- *
- *
- * uncomment this section in production mode to automatically import bootstrap with glyphicons,
- * FontAwesome,
- * Your Sass css or you normal css
- * This Project can be used for any other Front End Project apart from react all you just have to
- * do is to let this page end as your entry point
- *
- *
- import Bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css';
- import FontAwesome from '../node_modules/font-awesome/css/font-awesome.css';
- import styling from '../css/app.scss';
- *
- *
- import Bootstrap from '../../node_modules/bootstrap/dist/css/bootstrap.css';
- import FontAwesome from '../../node_modules/font-awesome/css/font-awesome.css';
- import styling from '../css/app.scss';
- *
- *
- *****/
-
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
@@ -28,8 +6,12 @@ import { createStore } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+import Bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css';
+import FontAwesome from '../node_modules/font-awesome/css/font-awesome.css';
+import styling from '../css/app.scss';
+
 ReactDOM.render(
-<Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers)}>
     <App />
-    </Provider>
+  </Provider>
     , document.getElementById('container'));
