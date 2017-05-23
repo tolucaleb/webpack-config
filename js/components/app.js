7c42dc7056c0ccde0a554a1395fe9445ca770f07
caleb tolulope <israel.fabulous@gmail.com>
2017-05-23 19:02:23 +0100

    showing support for react styling

--- js/components/app.js
+++ js/components/app.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
+const style = {
+  inlineStyle: {
+    color: '#3b5998',
+  },
+};
+
+const { inlineStyle } = style;
 const App = () => (
   <div className="row">
     <div className="marginalized-2x">
       <div className="main">
-   Webpack Config with React and hot Reloading <i className="fa fa-handshake-o" />
+        <p style={inlineStyle}>
+          Webpack Config with React and hot  Reloading <i className="fa fa-handshake-o" />
+        </p>
       </div>
     </div>
   </div>
     );
 
 export default App;
-
